8e16a1cbd922225019a57b1226260c8573bb85f5
Daniel Setyawan <artexsdns@gmail.com>
2022-09-14 16:09:58 +0700

    ok test koarmada

--- app/config/app.php
+++ app/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://172.22.0.1:8988/public'),
+    'url' => env('APP_URL', 'http://localhost:8988/public'),
 
     'asset_url' => env('ASSET_URL', null),
 
