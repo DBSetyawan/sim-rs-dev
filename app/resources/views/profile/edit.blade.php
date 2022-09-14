8fa3d2d656c0f0b55925643b6a238afd9ef1e4d3
Daniel Setyawan <artexsdns@gmail.com>
2022-09-14 23:37:41 +0700

    latest dev

--- app/resources/views/profile/edit.blade.php
+++ app/resources/views/profile/edit.blade.php
@@ -15,15 +15,15 @@
                         <div class="col-lg-3 order-lg-2">
                             <div class="card-profile-image">
                                 <a href="#">
-                                    <img src="{{ asset('argon') }}/img/theme/team-4-800x800.jpg" class="rounded-circle">
+                                    <img src="/imgs.jpeg" class="rounded-circle">
                                 </a>
                             </div>
                         </div>
                     </div>
                     <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
                         <div class="d-flex justify-content-between">
-                            <a href="#" class="btn btn-sm btn-info mr-4">{{ __('Connect') }}</a>
-                            <a href="#" class="btn btn-sm btn-default float-right">{{ __('Message') }}</a>
+                            <!-- <a href="#" class="btn btn-sm btn-info mr-4">{{ __('Connect') }}</a>
+                            <a href="#" class="btn btn-sm btn-default float-right">{{ __('Message') }}</a> -->
                         </div>
                     </div>
                     <div class="card-body pt-0 pt-md-4">
@@ -31,21 +31,21 @@
                             <div class="col">
                                 <div class="card-profile-stats d-flex justify-content-center mt-md-5">
                                     <div>
-                                        <span class="heading">22</span>
-                                        <span class="description">{{ __('Friends') }}</span>
+                                        <!-- <span class="heading">22</span>
+                                        <span class="description">{{ __('Friends') }}</span> -->
                                     </div>
                                     <div>
-                                        <span class="heading">10</span>
-                                        <span class="description">{{ __('Photos') }}</span>
+                                        <!-- <span class="heading">10</span>
+                                        <span class="description">{{ __('Photos') }}</span> -->
                                     </div>
                                     <div>
-                                        <span class="heading">89</span>
-                                        <span class="description">{{ __('Comments') }}</span>
+                                        <!-- <span class="heading">89</span>
+                                        <span class="description">{{ __('Comments') }}</span> -->
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="text-center">
+                        <!-- <div class="text-center">
                             <h3>
                                 {{ auth()->user()->name }}<span class="font-weight-light">, 27</span>
                             </h3>
@@ -61,7 +61,7 @@
                             <hr class="my-4" />
                             <p>{{ __('Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music.') }}</p>
                             <a href="#">{{ __('Show more') }}</a>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
